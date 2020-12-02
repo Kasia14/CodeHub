89e295b3b7418247ed605b8b88d0d2f21f45d81f
Kasia14 <katarzynatyszka14@gmail.com>
2020-12-02 13:18:02 +0100

    commit

--- trening/nauka zaawansowany javascript/błedyiwyjątki.js
+++ trening/nauka zaawansowany javascript/błedyiwyjątki.js
@@ -16,7 +16,7 @@ class CustomError extends Error {
 }
 
 throw new CustomError ('Message here');
-*/
+
 class CustomError extends Error {
     constructor(message){
         super(message);
@@ -31,4 +31,35 @@ try {
     else if(err.name === 'InternalError') console.log('Bład wew');
     else if (err.name === 'CustomError') console.log('Wystąpił własny błąd');
     else console.log('Wystąpił nieznany błąd');
+}
+*/
+// ćwiczenie 1
+
+class DivideByZero extends Error {
+    constructor(message, kod){
+        super(message);
+        this.name  = 'CustomError';
+        this.kod = kod;
+    }
+}
+
+function div(a, b){
+    if(b === 0)
+    {
+        throw new DivideByZero('Nie dziel debilu przez 0', 10)
+    }
+    else if(b!== Number(b)){
+        throw new DivideByZero('możesz dzielić tylko liczby', 11) 
+    }
+    return a/b;
+}
+
+
+function test()
+{
+    try{
+        div(10, 0);
+    }
+    catch (err){
+    }
 }
