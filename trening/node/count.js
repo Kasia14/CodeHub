cbb3b688ac0aae0e481f2dd3fe18ba217bba64de
Kasia14 <katarzynatyszka14@gmail.com>
2020-12-23 12:38:17 +0100

    commit

---
+++ trening/node/count.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const number = [6,1,37,2]
+
+fs.writeFileSync('out1.txt', number);
---
+++ trening/node/fileManipulation.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const name = "Tony";
+
+fs.writeFileSync('out.txt', name);
